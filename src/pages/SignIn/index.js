7e400b143bfab64d280593fa668c766ce314988b
SandyOliveira <sandyoliveira828@gmail.com>
2020-08-24 22:30:12 -0300

    Rotas privadas

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -2,8 +2,10 @@ import React from 'react';
 
 // import { Container } from './styles';
 
-function SignIn() {
-  return <div />;
+export default function SignIn() {
+  return (
+    <h1>Login</h1>
+  );
 }
 
-export default SignIn;
+
